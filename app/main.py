e279b94860487a2a66f46640272d25d781d49bfd
warfail <riskytaewa10@gmail.com>
2026-06-25 00:32:37 +0700

    fix: comment duplicate menu_boundary router

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from app.config.database import MongoDB
-from app.boundaries import menu_boundary, cashier_boundary, kitchen_boundary
+from app.boundaries import cashier_boundary, kitchen_boundary
 from app.routes import order_routes
 from app.routes import menu_routes
 from app.routes import inventory_routes
@@ -32,7 +32,7 @@ app.add_middleware(
 )
 
 # Router
-app.include_router(menu_boundary.router)
+
 app.include_router(cashier_boundary.router)
 app.include_router(kitchen_boundary.router)
 app.include_router(order_routes.router)  # Endpoint orders di sini
