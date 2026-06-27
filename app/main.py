90c9fcdd56cedf950c16b84545b5860ba66f163d
warfail <riskytaewa10@gmail.com>
2026-06-28 03:15:05 +0700

    fix: update CORS settings

--- app/main.py
+++ app/main.py
@@ -25,7 +25,14 @@ app = FastAPI(lifespan=lifespan, redirect_slashes=False)  # ← TAMBAHKAN INI
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://restoran-frontend-a8qg07czt-warfails-projects.vercel.app",
+        "https://restoran-frontend-a9e4ka72l-warfails-projects.vercel.app",
+        "https://restoran-frontend-a9e4ka72l-warfails-projects.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "*"  
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
