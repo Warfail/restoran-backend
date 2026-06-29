20ee71652bc03d8d75d68ef64260702e5d39f05c
warfail <riskytaewa10@gmail.com>
2026-06-29 17:22:28 +0700

    fix: add parse_json to app/utils/__init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,5 +1,3 @@
-cd restorant-backend
-cat > app/utils/__init__.py << 'EOF'
 from bson import ObjectId
 import json
 from datetime import datetime
@@ -24,28 +22,4 @@ def serialize_document(doc):
 def serialize_value(value):
     if isinstance(value, ObjectId):
         return str(value)
-    elif isinstance(value, datetime):
-        return value.isoformat()
-    elif isinstance(value, list):
-        return [serialize_value(item) for item in value]
-    elif isinstance(value, dict):
-        return serialize_document(value)
-    else:
-        return value
-
-def serialize_list(docs):
-    return [serialize_document(doc) for doc in docs] if docs else []
-
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, ObjectId):
-            return str(obj)
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return super().default(obj)
-
-def parse_json(data):
-    if data is None:
-        return None
-    return json.loads(json.dumps(data, cls=CustomJSONEncoder))
-EOF
+    elif isinstance(value, datetime
